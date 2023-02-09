7db425d6ddd187eb627808a6436d5d687cf75fc4
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2023-02-09 21:09:37 +0200

    Initialised project and added files to .gitignore

---
+++ src/main/java/com/project/trackfit/TrackfitApplication.java
@@ -0,0 +1,13 @@
+package com.project.trackfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/trackfit/TrackfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.trackfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
