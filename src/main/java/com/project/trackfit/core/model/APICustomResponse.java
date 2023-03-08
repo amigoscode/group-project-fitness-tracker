ff89d2e6a02e2e860954433a25220526203391e7
Moe Alshidi <mohammedalshidi55@gmail.com>
2023-03-08 14:25:24 +0400

    resolve pull request feedback

--- src/main/java/com/project/trackfit/core/model/APICustomResponse.java
+++ src/main/java/com/project/trackfit/core/model/APICustomResponse.java
@@ -6,7 +6,6 @@ import lombok.experimental.SuperBuilder;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
-import java.util.Map;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
@@ -15,12 +14,12 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 @JsonInclude(NON_NULL)
 public class CustomResponse {
     protected LocalDateTime timeStamp;
-    protected  int statusCode;
+    protected int statusCode;
     protected HttpStatus status;
-    protected  String reason;
-    protected  String message;
+    protected String reason;
+    protected String message;
     protected String developerMessage;
 
-    protected Map<?,?> data;
+    protected Object data;
 
 }
