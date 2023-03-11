d9f10b39f10c914eecb0b090d9feb923df3dcf03
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2023-03-11 15:49:33 +0200

    Added GlobalExceptionHandlerTests, modified ErrorResponse

---
+++ src/test/java/com/project/trackfit/core/exception/GlobalExceptionHandlerTests.java
@@ -0,0 +1,58 @@
+package com.project.trackfit.core.exception;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class GlobalExceptionHandlerTests {
+
+    @Mock
+    private ResourceNotFoundException resourceNotFoundException;
+
+    @Mock
+    private EmailNotValidException emailNotValidException;
+
+    @Mock
+    private EmailAlreadyTakenException emailAlreadyTakenException;
+
+    @InjectMocks
+    private GlobalExceptionHandler globalExceptionHandler;
+
+    @Test
+    @DisplayName("Check that ResourceNotFoundException returns corresponding error response")
+    public void handleResourceNotFoundException_shouldReturnErrorResponse() {
+        when(resourceNotFoundException.getMessage()).thenReturn("User Doesn't Exist");
+        ErrorResponse response = globalExceptionHandler.handleResourceNotFoundException(resourceNotFoundException);
+        assertEquals(HttpStatus.NOT_FOUND, response.getHttpStatus());
+        assertEquals("User Doesn't Exist", response.getMessage());
+        assertEquals("User Doesn't Exist", response.getEx().getMessage());
+    }
+
+    @Test
+    @DisplayName("Check that EmailNotValidException returns corresponding error response")
+    public void handleEmailNotValidException_shouldReturnErrorResponse() {
+        when(emailNotValidException.getMessage()).thenReturn("Invalid email");
+        ErrorResponse response = globalExceptionHandler.handleEmailNotValidException(emailNotValidException);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getHttpStatus());
+        assertEquals("Email not valid", response.getMessage());
+        assertEquals("Invalid email", response.getEx().getMessage());
+    }
+
+    @Test
+    @DisplayName("Check that EmailAlreadyTakenException returns corresponding error response")
+    public void handleEmailAlreadyTakenException_shouldReturnErrorResponse() {
+        when(emailAlreadyTakenException.getMessage()).thenReturn("Email already taken");
+        ErrorResponse response = globalExceptionHandler.handleEmailAlreadyTakenException(emailAlreadyTakenException);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getHttpStatus());
+        assertEquals("Email already taken", response.getMessage());
+        assertEquals("Email already taken", response.getEx().getMessage());
+    }
+}
