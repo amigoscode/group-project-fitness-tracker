e0790b5dda3d009580425c069386ffd1282aa9c1
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2023-12-20 20:32:17 +0200

    Added more tests in CustomerControllerTests

--- src/test/java/com/project/trackfit/customer/controller/CustomerControllerTests.java
+++ src/test/java/com/project/trackfit/customer/controller/CustomerControllerTests.java
@@ -1,6 +1,9 @@
 package com.project.trackfit.customer.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.trackfit.core.exception.ResourceNotFoundException;
 import com.project.trackfit.customer.dto.Customer;
+import com.project.trackfit.customer.dto.UpdateCustomerRequest;
 import com.project.trackfit.customer.service.ICustomerService;
 import com.project.trackfit.security.jwt.JwtRequestFilter;
 import com.project.trackfit.user.dto.CreateUserRequest;
@@ -14,6 +17,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -22,8 +26,10 @@ import java.util.UUID;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -48,9 +54,12 @@ public class CustomerControllerTests {
     @MockBean
     private IApplicationUserService userService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     @DisplayName("Successfully get a customer by his unique Id")
-    public void givenUserRequest_whenCreateUser_thenReturnSavedUser() throws Exception {
+    public void givenCustomer_whenGetCustomerById_thenReturnCustomer() throws Exception {
         //given: a random UUID
         UUID customer_id = UUID.randomUUID();
 
@@ -73,6 +82,61 @@ public class CustomerControllerTests {
                 .andExpect(jsonPath("$.timeStamp", notNullValue()))
                 .andExpect(jsonPath("$.statusCode", is(200)))
                 .andExpect(jsonPath("$.message", is("Customer has been fetched successfully")))
+                .andExpect(jsonPath("$.data", notNullValue()));
+    }
+
+    @Test
+    @DisplayName("Getting a customer fails because he doesn't exist")
+    public void givenNonExistingCustomerUUID_whenGetCustomerById_thenReturnIsNotFound() throws Exception {
+        //given: a non-existent customer id
+        UUID nonExistentCustomerId = UUID.randomUUID();
+
+        //and: mocking the service to return the proper exception
+        given(service.getCustomerById(nonExistentCustomerId)).willThrow(new ResourceNotFoundException());
+
+        //when: trying to fetch this customer
+        ResultActions response = mockMvc.perform(get("/api/v1/customers/{id}", nonExistentCustomerId));
+
+        //then: the response is Not Found
+        response.andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message", is("User Doesn't Exist")));
+    }
+
+    @DisplayName("Successfully update a customer")
+    @Test
+    public void givenSavedCustomer_whenUpdateCustomer_thenReturnUpdatedCustomer() throws Exception {
+        //given: a random UUID
+        UUID customer_id = UUID.randomUUID();
+
+        //and: creating a random customer
+        EasyRandom easyRandom = new EasyRandom();
+        Customer savedCustomer = easyRandom.nextObject(Customer.class);
+
+        //and: an updated customer
+        Customer updatedCustomer = easyRandom.nextObject(Customer.class);
+
+        //and: mocking the service to create a user with is UUID
+        given(userService.createUser(any(CreateUserRequest.class))).willAnswer((invocation) -> customer_id);
+
+        //and: mocking the service to return this random customer
+        given(service.getCustomerById(customer_id)).willReturn(savedCustomer);
+
+        //and: mocking the service to update this customer
+        given(service.updateCustomer(eq(customer_id), any(UpdateCustomerRequest.class)))
+                .willReturn(updatedCustomer);
+
+        //when: sending the request
+        ResultActions response = mockMvc.perform(put("/api/v1/customers/{id}", customer_id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedCustomer)));
+
+        //then: status is OK, with proper expected response fields
+        response.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.timeStamp", notNullValue()))
+                .andExpect(jsonPath("$.statusCode", is(200)))
+                .andExpect(jsonPath("$.message", is("Customer has been updated successfully")))
                 .andExpect(jsonPath("$.data", notNullValue()));
     }
 }
